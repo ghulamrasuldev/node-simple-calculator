b77017d35fd8b38476d51c6b2549d56f57efe7eb
ghulamrasuldev <ghulam.rasool.development@gmail.com>
2022-09-23 15:13:16 +0500

    added post request with params

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 //jshint esversion:6
 
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
-app.get("/contacts", function (req, res) {
-  res.sendFile(__dirname + "/index.html");
+app.post("/", function (req, res) {
+  var result = Number(req.body.num1) + Number(req.body.num2);
+  res.send("Result is: " + result);
 });
 
 app.listen(4000);
